b3688c01da09d268f9e9171989a543b523cc1aff
Peter Law <PeterJCLaw@gmail.com>
2013-10-11 12:33:41 +0100

    Add scripts to help create kickstart users.
    getteamsdata extracts the team data from trac, outputting it to a
    file (teams_map.csv) for storage and inspection.
    
    kickstartusers creates a user per team from the teams_map.csv file,
    ensuring that this user is a student, and is in the right team and
    college groups (which it creates if needed).
    
    Change-Id: I69390b6a0a59f2e2eea66fbd17738471fe21c8b9

---
+++ getteamsdata.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import urllib
+
+TEAMS_PAGE = 'https://www.studentrobotics.org/trac/wiki/2014/Teams?format=txt'
+
+tmpfile, headers = urllib.urlretrieve(TEAMS_PAGE)
+
+lines = []
+
+with open(tmpfile) as f:
+    for line in f:
+        line = line.strip()
+
+        if line == '= Registered Schools =':
+            store_lines = True
+            continue
+        if line.startswith('='):
+            store_lines = False
+            continue
+
+        if store_lines and len(line) > 0:
+            lines.append(line)
+
+team_map = {}
+
+# ignore the header
+for line in lines[1:]:
+    if not line.startswith('||'):
+        break
+    parts = line.split('||', 4)
+    x, tla, college_name, rest = [parts[i].strip() for i in xrange(4)]
+    team_map[tla] = college_name
+
+for tla, college_name in team_map.iteritems():
+    print "{0},{1}".format(tla, college_name)
---
+++ kickstartusers.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+import sr
+from c_teams import get_college, get_team
+
+TEAMS_MAP_FILE = 'teams_map.csv'
+KICKSTART_USER_PREFIX = 'kick_'
+
+def get_teams():
+    with open(TEAMS_MAP_FILE) as f:
+        for line in f:
+            tla, name = line.strip().split(',')
+            yield tla, name
+
+def ensure_college(tla, name, user):
+    grp = get_college(tla[0:3])
+    if not grp.in_db:
+        grp.desc = name
+
+    grp.user_add(user)
+    grp.save()
+
+def ensure_team(tla, user):
+    grp = get_team(tla)
+    grp.user_add(user)
+    grp.save()
+
+def create_team_user(tla, college_name):
+    tu = sr.user(KICKSTART_USER_PREFIX + tla)
+    tu.cname = tla
+    tu.sname = college_name
+    tu.email = ''
+    tu.save()
+    return tu
+
+def set_password(tu):
+    passwd = sr.users.GenPasswd()
+    tu.passwd = passwd
+    tu.save()
+    return passwd
+
+def create_all(team_source):
+    students_group = sr.group('students')
+
+    passwds = {}
+
+    for tla, college_name in team_source:
+        tu = create_team_user(tla, college_name)
+        passwds[tu.username] = set_password(tu)
+
+        ensure_college(tla, college_name, tu)
+        ensure_team(tla, tu)
+        students_group.user_add(tu)
+
+    students_group.save()
+    return passwds
+
+if __name__ == '__main__':
+    passwds_map = create_all(get_teams())
+    for u, p in passwds_map.iteritems():
+        print u, p
